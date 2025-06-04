2493ba515ca35287a696fd13d26de8a12e15dd0c
create-to-solve <ashish.george@aya.yale.edu>
2025-06-04 04:38:04 +0100

    Create climate_vectorizer.py

---
+++ scripts/climate_vectorizer.py
@@ -0,0 +1,8 @@
+# Stub for climate project vectorization
+
+def vectorize_project(text):
+    """
+    Takes a raw climate project description as input,
+    returns a vector representation using predefined symbolic encoders.
+    """
+    pass
